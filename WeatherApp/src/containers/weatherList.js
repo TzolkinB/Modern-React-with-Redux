86c75b46ff1c8425520259e7c67c8edceff619ef
Kimberly Bell <kimberly.bell@tma1.com>
2017-03-25 22:03:33 -0400

    Added react-sparklines and used it to show city temps as reusable line chart component

--- WeatherApp/src/containers/weatherList.js
+++ WeatherApp/src/containers/weatherList.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import Chart from '../components/chart';
 
 class WeatherList extends React.Component {
 	renderWeather(cityData) {
 		const name = cityData.city.name;
+		const temps = cityData.list.map(weather => weather.main.temp);
+		const humidity = cityData.list.map(weather => weather.main.humidity);
+		const pressure = cityData.list.map(weather => weather.main.humidity);
+
 		return (
 			<tr key={name}>
 				<td>{name}</td>
+				<td>	
+					<Chart data={temps} color="orange" />
+				</td>
 			</tr>
 		)
 	}
