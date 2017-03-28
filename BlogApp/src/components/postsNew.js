0bb990574a73ccd296f5f283acb65e74678dcf35
Kimberly Bell <kimberly.bell@tma1.com>
2017-03-28 17:55:03 -0400

    Wire up 3 inputs to redux-form

--- BlogApp/src/components/postsNew.js
+++ BlogApp/src/components/postsNew.js
@@ -3,24 +3,27 @@ import {reduxForm} from 'redux-form';
 
 class PostsNew extends React.Component {
 	render() {
+		//handleSubmit is a function given to us by redux-form
+		const { fields: { title, categories, content }, handleSubmit } = this.props
+		
 		return(
 			<div>
 
-				<form>
+				<form onSubmit={handleSubmit}>
         <h3>Create A New Post</h3>
         <div className="form-group">
           <label>Title</label>
-          <input type="text" className="form-control" />
+          <input type="text" className="form-control" {...title} />
         </div>
 
         <div className="form-group">
           <label>Categories</label>
-          <input type="text" className="form-control" />
+          <input type="text" className="form-control" {...categories} />
         </div>
 
         <div className="form-group">
           <label>Content</label>
-          <textarea className="form-control" />
+          <textarea className="form-control" {...content} />
         </div>
 
         <button type="submit" className="btn btn-primary">Submit</button>
