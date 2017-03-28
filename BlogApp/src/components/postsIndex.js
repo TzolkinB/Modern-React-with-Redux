55cb04b19d25f8766e89fee5f0c9df887ff3d828
Kimberly Bell <kimberly.bell@tma1.com>
2017-03-28 16:18:35 -0400

    Added mapDispatchToProps to access posts and can test by seeing post request in browser dev tools

--- BlogApp/src/components/postsIndex.js
+++ BlogApp/src/components/postsIndex.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/index';
+import {bindActionCreators} from 'redux';
+//import { Link } from 'react-router';
 
-class PostsShow extends React.Component {
+class PostsIndex extends React.Component {
+
+	componentWillMount () {
+		// call the action creator, made accessible by mapDispatchToProps, to fetch posts
+		// to test at this point, look in "Network" tab of dev tools to see a post request
+		this.props.fetchPosts();
+	}
+
 	render() {
 		return(
 			<div>
@@ -10,4 +21,8 @@ class PostsShow extends React.Component {
 	}
 }
 
-export default PostsShow;
+const mapDispatchToProps = (dispatch) => {
+	return bindActionCreators({ fetchPosts }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(PostsIndex);
