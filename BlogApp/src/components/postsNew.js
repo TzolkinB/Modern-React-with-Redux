beb474f9cbba71236a8f3e24d435ba90a632fa93
Kimberly Bell <kimberly.bell@tma1.com>
2017-04-04 22:02:09 -0400

    Add cancel button and validations from redux-form

--- BlogApp/src/components/postsNew.js
+++ BlogApp/src/components/postsNew.js
@@ -1,34 +1,63 @@
 import React from 'react';
 import {reduxForm} from 'redux-form';
+import {Link} from 'react-router';
 import {createPost} from '../actions/index';
 
+function validate(values) {
+  const errors = {};
+  if(!values.title) {
+    errors.title = "Enter a title";
+  }
+  if(!values.categories) {
+    errors.categories = "Enter a category";
+  }
+  if(!values.content) {
+    errors.content = "Enter some content";
+  }
+  return errors;
+}
+
 class PostsNew extends React.Component {
+
 	render() {
 		//handleSubmit is a function given to us by redux-form
 		const { fields: { title, categories, content },
       handleSubmit, onSubmit} = this.props
 
-		console.log(this.props);
+    const titleValid = title.touched && title.invalid ? 'has-danger' : '';
+    const categoriesValid = categories.touched && categories.invalid ? 'has-danger' : '';
+    const contentValid = content.touched && content.invalid ? 'has-danger' : '';
+
 		return(
 			<div>
         <form onSubmit={handleSubmit(this.props.createPost)}>
           <h3>Create A New Post</h3>
-          <div className="form-group">
+          <div className={`form-group ${titleValid}`}>
             <label>Title</label>
             <input type="text" className="form-control" {...title} />
+            <div className="text-help">
+              {title.touched ? title.error : ''}
+            </div>
           </div>
 
-          <div className="form-group">
+          <div className={`form-group ${categoriesValid}`}>
             <label>Categories</label>
             <input type="text" className="form-control" {...categories} />
+            <div className="text-help">
+              {categories.touched ? categories.error : ''}
+            </div>
           </div>
 
-          <div className="form-group">
+          <div className={`form-group ${contentValid}`}>
             <label>Content</label>
             <textarea className="form-control" {...content} />
+            <div className="text-help">
+              {content.touched ? content.error : ''}
+            </div>
           </div>
 
-          <button type="submit" className="btn btn-primary">Submit</button>
+          <button type="submit" className="btn btn-primary button-margin">Submit</button>
+          <Link to="/" className="btn btn-danger button-margin">Cancel</Link>
         </form>
 
 			</div>
@@ -44,7 +73,8 @@ class PostsNew extends React.Component {
 PostsNew = reduxForm({
 	// pass in what form is going to be called as well as fields
 	form: 'PostsNewForm',
-  fields: ['title', 'categories', 'content']
+  fields: ['title', 'categories', 'content'],
+  validate
 }, null, {createPost})(PostsNew);
 
 export default PostsNew;
