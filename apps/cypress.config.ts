08ec3865715afeb7c736200e66a9c75d59a198da
TzolkinB <krbell4@gmail.com>
2024-02-01 18:00:32 -0500

    npm install various cypress and vite scripts

---
+++ apps/cypress.config.ts
@@ -0,0 +1,18 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { defineConfig } from "cypress"
+
+export default defineConfig({
+  e2e: {
+    baseUrl: "http://localhost:3030",
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "vite",
+    },
+  },
+})
