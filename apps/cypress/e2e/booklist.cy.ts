6f22441fd511c7e557d5fb3dd6ce47d80c3976ed
TzolkinB <krbell4@gmail.com>
2024-02-01 19:30:02 -0500

    fix more ts linting issues with cypress findByRole still not a function

---
+++ apps/cypress/e2e/booklist.cy.ts
@@ -0,0 +1,14 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+// import { findByRole } from "@testing-library/cypress"
+
+describe("booklist app", () => {
+  it("should load the page", () => {
+    cy.intercept("localhost:3030").as("localhost")
+    cy.visit("/")
+    cy.wait("@localhost")
+    cy.get("header").findByRole("tablist")
+    //   .findByRole("tab", { name: "Booklist" })
+    //   .click()
+    // cy.url().should("include", "/booklist")
+  })
+})
